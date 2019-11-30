a4c1bb4f3cb417a28dc7a9315bee27a2abf3e256
Dennis Lee <v-dele@microsoft.com>
2019-11-29 20:56:23 -0800

    Completed the activity.

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import json
 
 import requests
 
-# Use Like python githubber.py JASchilz
+# Use Like python main.py JASchilz
 # (or another user name)
 
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    # Getting the timestamp for the first event for the user specified.
+    # By first event, I'm interpreting that as the earliest event.
+    # Since the events in the API response appear to be ordered from newest to
+    # oldest, I'm picking out the timestamp for the event at the very end of the
+    # JSON output. (Although it appears that the list of events is restricted
+    # to a certain number and doesn't go to the very beginning of the user
+    # history.)
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    tstamp = response.json()[-1]['created_at']
 
-    print("COMPLETE THE TODOs")
+    print(tstamp)
     
 
 
